cda1840012852f7df9c6c513c75165e7f3a64ef7
Griffin Milsap <griffin.milsap@gmail.com>
2011-09-29 17:49:34 -0400

    added skeleton code and project stuff for client and server side

---
+++ server/src/main.cpp
@@ -0,0 +1,20 @@
+#include <QtGui>
+#include "MainWindow.h"
+
+int main( int argc, char *argv[] )
+{
+	Q_INIT_RESOURCE( RerootResources );
+	
+	QApplication a(argc, argv);
+	
+	// Some error checking -- Some OS's don't support systrays.
+	if( !QSystemTrayIcon::isSystemTrayAvailable() ) {
+		QMessageBox::critical( 0, QObject::tr( "Systray" ), QObject::tr( "I couldn't detect any system tray on this system." ) );
+		return 1;
+	}
+	QApplication::setQuitOnLastWindowClosed( false );
+	
+	MainWindow w;
+	w.show();
+	return a.exec();
+}
