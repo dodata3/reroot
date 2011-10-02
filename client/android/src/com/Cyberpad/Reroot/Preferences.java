918900e9ebb9c296e5309d1be2e7708bd73e6514
Beth Werbaneth <werbae@rpi.edu>
2011-10-02 02:54:19 -0400

    added functionality for saving ip address between uses

---
+++ client/android/src/com/Cyberpad/Reroot/Preferences.java
@@ -0,0 +1,16 @@
+package com.Cyberpad.Reroot;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity{
+	//called when the activity is first created
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+	
+
+}
