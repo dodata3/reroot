4611aac0c55ba61dc2e1aa37e27c007a07fa74f1
Beth Werbaneth <werbae@rpi.edu>
2011-10-02 02:12:07 -0400

    :~) basic application ui is a go

--- client/android/src/com/Cyberpad/Reroot/RerootActivity.java
+++ client/android/src/com/Cyberpad/Reroot/RerootActivity.java
@@ -2,12 +2,42 @@ package com.Cyberpad.Reroot;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
 
 public class RerootActivity extends Activity {
+	private EditText text;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        text = (EditText)findViewById(R.id.editText1);
+        //TextView tv = new TextView(this);
+        //tv.setText("Hello, Android!");
+        //setContentView(tv);
     }
+    
+    //this method is called at button click
+    public void myClickHandler(View view){
+    	switch(view.getId()){
+    	case R.id.button1:
+    		if(text.getText().length() == 0){
+    			Toast.makeText(this, "Please enter a valid number", 
+    					Toast.LENGTH_LONG).show();
+    			return;
+    		}
+    		
+    		float inputValue = Float.parseFloat(text.getText().toString());
+    		
+    		Toast.makeText(this, "Message received", Toast.LENGTH_LONG).show();
+    			
+    	}
+    }
+   
 }
