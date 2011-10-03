6364ed4568fafef01790ce3db079ba45d62f8e98
Beth Werbaneth <werbae@rpi.edu>
2011-10-02 23:13:29 -0400

    Fixed ip address input

--- client/android/gen/com/Cyberpad/Reroot/R.java
+++ client/android/gen/com/Cyberpad/Reroot/R.java
@@ -17,10 +17,17 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int LinearLayout1=0x7f070000;
+        public static final int TextView01=0x7f070006;
+        public static final int TextView02=0x7f070003;
+        public static final int TextView03=0x7f070008;
         public static final int button1=0x7f070002;
-        public static final int button2=0x7f070003;
-        public static final int editText1=0x7f070001;
-        public static final int textView1=0x7f070000;
+        public static final int button2=0x7f070001;
+        public static final int editText1=0x7f070004;
+        public static final int editText2=0x7f070007;
+        public static final int editText3=0x7f070005;
+        public static final int editText4=0x7f07000a;
+        public static final int textView1=0x7f070009;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -29,6 +36,10 @@ public final class R {
         public static final int app_name=0x7f050001;
         public static final int enterBtn=0x7f050004;
         public static final int hello=0x7f050000;
+        public static final int ip_four=0x7f050009;
+        public static final int ip_one=0x7f050006;
+        public static final int ip_three=0x7f050008;
+        public static final int ip_two=0x7f050007;
         public static final int myClickHandler=0x7f050003;
         public static final int my_ip_address=0x7f050002;
         public static final int showBtn=0x7f050005;
