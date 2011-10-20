c723b70a4a48abccf60ce58d70854d71c3c7ab23
Beth Werbaneth <werbae@rpi.edu>
2011-10-20 00:24:03 -0400

    keyboard should now be sending messages

--- client/android/src/com/Cyberpad/Reroot/PadActivity.java
+++ client/android/src/com/Cyberpad/Reroot/PadActivity.java
@@ -12,11 +12,20 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.KeyCharacterMap;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnKeyListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.view.KeyEvent;
 
 
 public class PadActivity extends Activity {
@@ -27,6 +36,9 @@ public class PadActivity extends Activity {
 	//private boolean softShown = false;
 	private Runnable rMidDown;
 	private Runnable rMidUp;
+	public static KeyCharacterMap charmap;
+	
+	private EditText AdvancedText;
 	
 	SharedPreferences preferences;
 	private static final String TAG = "Reroot";
@@ -38,26 +50,17 @@ public class PadActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
+	    //set display to our new layout
 	    setContentView(R.layout.new_layout);
+	    /*requestWindowFeature(Window.FEATURE_NO_TITLE);
+	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+	    		WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
 	    //get preferences
 	    preferences = PreferenceManager.getDefaultSharedPreferences(this);
-	    FrameLayout touchpad = (FrameLayout)findViewById(R.id.TouchPad);
-	    FrameLayout keys = (FrameLayout)findViewById(R.id.keyboard_btn);
-	    
-	    keys.setOnTouchListener(new View.OnTouchListener(){
-	    	public boolean onTouch(View v, MotionEvent ev){
-	    		return onKeyTouch(ev);
-	    	}
-	    });
-	    soft_keys = keys;
+	    charmap = KeyCharacterMap.load(KeyCharacterMap.BUILT_IN_KEYBOARD);
 	    
 	    
 	    
-		touchpad.setOnTouchListener(new View.OnTouchListener(){
-			public boolean onTouch(View v, MotionEvent ev){
-				return onMouseMove( ev );
-        	}
-        });
 	    
 		//initialize keyboard stuff
 		this.rMidDown = new Runnable() {
@@ -76,6 +79,10 @@ public class PadActivity extends Activity {
 	    	this.sender = new OSCPortOut(InetAddress.getByName(preferences.getString("ip_address", "n/a")),
 	    			OSCPort
 	    			.defaultSCOSCPort());
+	    	//initialize the touch pad and keyboard button
+	    	this.initTouchpad();
+	    	this.initKeys();
+	    	this.initAdvancedText();
 	    	
 	    }
 	    catch(Exception ex){
@@ -86,6 +93,90 @@ public class PadActivity extends Activity {
 	
 	    // TODO Auto-generated method stub
 	}
+	
+	//initializations
+	private void initTouchpad(){
+		FrameLayout touchpad = (FrameLayout)this.findViewById(R.id.TouchPad);
+		
+		touchpad.setOnTouchListener(new View.OnTouchListener(){
+			public boolean onTouch(View v, MotionEvent ev){
+				return onMouseMove( ev );
+        	}
+        });
+		
+	}
+	
+	private void initKeys(){
+	    FrameLayout keys = (FrameLayout)this.findViewById(R.id.keyboard_btn);
+	    
+	    keys.setOnTouchListener(new View.OnTouchListener(){
+	    	public boolean onTouch(View v, MotionEvent ev){
+	    		return onKeyTouch(ev);
+	    	}
+	    });
+	    
+	    this.soft_keys = keys;
+		
+	}
+	
+	String changed = "";
+	private void initAdvancedText(){
+		EditText et = (EditText)this.findViewById(R.id.AdvancedText);
+		this.AdvancedText = et;
+		
+		//prevent keyboard from going fullscreen in landscape
+		et.setImeOptions(EditorInfo.IME_FLAG_NO_EXTRACT_UI);
+		
+		changed = "a ";
+		AdvancedText.setText(changed);
+		
+		//listener
+		et.setOnKeyListener(new OnKeyListener(){
+			
+			//@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event){
+				changed = "a ";
+				AdvancedText.setText(changed);
+				return false;
+			}
+			
+		});
+		et.addTextChangedListener(new TextWatcher(){
+			public void onTextChanged(CharSequence s, int start, int before, int count){
+				if(s.toString().equals(changed)){
+					AdvancedText.requestFocus();
+					AdvancedText.setSelection(2);
+					return;
+				}
+				changed = null;
+				
+				String change = s.toString().substring(start, start+count);
+				
+				if(count != 0){
+					if(change.equals(" "))
+						sendKey(62);
+					else
+						sendKeys(change);	
+				}
+				else
+					sendKey(67);
+				
+				changed = "a ";
+				AdvancedText.setText(changed);
+			}
+			
+			public void afterTextChanged(Editable s){
+				
+			}
+			
+			public void beforeTextChanged(CharSequence s, int start, int count, int after){
+				
+			}
+			
+		});
+		
+	}
+	
 	
 	//mouse zone
 	
@@ -172,8 +263,155 @@ public class PadActivity extends Activity {
 	}
 
 	private void drawSoftOff() {
-		this.soft_keys.setBackgroundResource(R.drawable.keyboard_off);
+		this.soft_keys.setBackgroundResource(R.drawable.keyboard);
 	}
+	
+	private void sendKey(int keycode){
+		try{
+			//scoping brackets
+			{
+				Object[] args = new Object[3];
+				args[0] = 0; //key down
+				args[1] = keycode;
+				args[2] = new Character(Character.toChars(PadActivity.charmap.get(keycode,  0))[0]).toString();
+				OSCMessage msg = new OSCMessage("/keyboard", args);
+				
+				this.sender.send(msg);
+			}
+			{
+				Object[] args = new Object[3];
+				args[0] = 1; //key up
+				args[1] = keycode;
+				args[2] = new Character(Character.toChars(PadActivity.charmap.get(keycode,  0))[0]).toString();
+				OSCMessage msg = new OSCMessage("/keyboard", args);
+				
+				this.sender.send(msg);
+			}
+		}
+		catch(Exception ex){
+			Log.d(TAG, ex.toString());
+		}
+	}
+	
+	private void sendKeys(String keys){
+		if(keys.equals("a ")) return;
+		
+		for(int i=0; i<keys.length();i++){
+			String c = keys.substring(i, i+1);
+			boolean isShift = false;
+			boolean isCtrl = false;
+			
+			if(!c.toLowerCase().equals(c)){
+				isShift = true;
+				c = c.toLowerCase();
+			}
+			
+			int key = 0;
+			
+			//conversion zone
+			if(c.equals(" ")) key = 62;
+			if(c.equals("\n")) key = 66;
+			if(c.equals("\t")){key = 45; isCtrl = true;}
+			if (c.equals("_")){key = 95; isShift = true;}
+			if (c.equals("\"")){key = 75; isShift = true;}
+			if (c.equals("^")){key = 94; isShift = true;}
+			if (c.equals("~")){key = 126; isShift = true;}
+			if (c.equals("`")){key = 68; isShift = true;}
+			if (c.equals(":")){key = 74; isShift = true;}
+			if (c.equals("=")) key = 70;
+			if (c.equals("+")){key = 70; isShift=true;}
+			if (c.equals("%")){key = 12; isShift=true;}
+			if (c.equals("&")){key = 14; isShift=true;}
+			if (c.equals("^")){key = 13; isShift=true;}
+			if (c.equals("|")){key = 73; isShift=true;}
+			if (c.equals("_")){key = 69; isShift=true;}
+			if (c.equals("?")){key = 76; isShift=true;}
+			if (c.equals("!")){key = 8; isShift=true;}
+			if (c.equals("$")){key = 11; isShift=true;}
+			if (c.equals("~")){key = 68; isShift=true;}
+			if (c.equals("<")){key = 55; isShift=true;}
+			if (c.equals(">")){key = 56; isShift=true;}
+			if (c.equals("")){key = 56; isCtrl=true;}
+			if (c.equals("(")){key = 16; isShift = true;}
+			if (c.equals(")")){key = 7; isShift = true;}
+			if (c.equals("{")){key = 71; isShift = true;}
+			if (c.equals("}")){key = 72; isShift = true;}
+			if (c.equals("["))key = 71;
+			if (c.equals("]"))key = 72;
+			
+			if(key==0)
+				for(int z =0; z <1024; z++)
+					if(PadActivity.charmap.isPrintingKey(z))
+						if(new Character(Character.toChars(PadActivity.charmap
+								.get(z, 0))[0]).toString().equals(c)){
+							key = z;
+							break;
+						}
+			
+			try{
+				if(isCtrl){
+					Object[] args = new Object[3];
+					args[0] = 0; //key down
+					args[1] = 57;
+					args[2] = new Character ((char)0).toString();
+					OSCMessage msg = new OSCMessage("/keyboard", args);
+					
+					this.sender.send(msg);
+				}
+				if(isShift){
+					Object[] args = new Object[3];
+					args[0] = 0; //key down
+					args[1] = 59;
+					args[2] = new Character ((char)0).toString();
+					OSCMessage msg = new OSCMessage("/keyboard", args);
+					
+					this.sender.send(msg);
+				}
+				{
+					Object[] args = new Object[3];
+					args[0] = 0; //key down
+					args[1] = key;
+					args[2] = c;
+					OSCMessage msg = new OSCMessage("/keyboard", args);
+					
+					this.sender.send(msg);
+				}
+				{
+					Object[] args = new Object[3];
+					args[0] = 1; /* key up */
+					args[1] = key;// (int)c;
+					args[2] = c;
+					OSCMessage msg = new OSCMessage("/keyboard", args);
+	
+					this.sender.send(msg);
+				}
+
+				if(isShift){
+					Object[] args = new Object[3];
+					args[0] = 1; /* key up */
+					args[1] = 59;// (int)c;
+					args[2] = new Character((char)0).toString();
+					OSCMessage msg = new OSCMessage("/keyboard", args);
+
+					this.sender.send(msg);
+				}
+				
+				if(isCtrl){
+					Object[] args = new Object[3];
+					args[0] = 1; /* key up */
+					args[1] = 57;// (int)c;
+					args[2] = new Character((char)0).toString();
+					OSCMessage msg = new OSCMessage("/keyboard", args);
+
+					this.sender.send(msg);
+				}
+			}
+			catch(Exception ex){
+				Log.d(TAG, ex.toString());
+			}
+		}
+	}
+	
 
 }
 
--- client/android/src/com/Cyberpad/Reroot/RerootActivity.java
+++ client/android/src/com/Cyberpad/Reroot/RerootActivity.java
@@ -6,7 +6,6 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.widget.TextView;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
