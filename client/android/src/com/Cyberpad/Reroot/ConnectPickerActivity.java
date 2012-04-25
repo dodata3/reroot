060da1d80abb68e9ff10f303393b470273a62d84
Griffin Milsap <griffin.milsap@gmail.com>
2012-04-25 13:11:31 -0400

    Using ZXing for QR scanning now

--- client/android/src/com/Cyberpad/Reroot/ConnectPickerActivity.java
+++ client/android/src/com/Cyberpad/Reroot/ConnectPickerActivity.java
@@ -1,12 +1,19 @@
 package com.Cyberpad.Reroot;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import com.Cyberpad.RerootService.RerootService;
 import com.Cyberpad.RerootService.RerootBinder;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
@@ -38,6 +45,16 @@ public class ConnectPickerActivity extends Activity {
 		}
 	};
 	
+	private BroadcastReceiver mAuthReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO Auto-generated method stub
+			Intent menuIntent = new Intent( ConnectPickerActivity.this, MainMenuActivity.class);
+			startActivity( menuIntent );
+			finish();
+		}
+	};
+	
 	@Override
 	public void onCreate( Bundle icicle ) {
 		super.onCreate( icicle );
@@ -53,8 +70,10 @@ public class ConnectPickerActivity extends Activity {
 		qr_button.setOnClickListener(new OnClickListener() {
 			public void onClick(View v){
 				try{
-					Intent i = new Intent( ConnectPickerActivity.this, QRConnectActivity.class );
-					ConnectPickerActivity.this.startActivity(i);
+					IntentIntegrator integrator = new IntentIntegrator( ConnectPickerActivity.this );
+					integrator.initiateScan();
+					//Intent i = new Intent( ConnectPickerActivity.this, QRConnectActivity.class );
+					//ConnectPickerActivity.this.startActivity(i);
 				}
 				catch(Exception ex){
 					Log.e( "Reroot", "Could not start the QRConnectActivity" );
@@ -83,6 +102,34 @@ public class ConnectPickerActivity extends Activity {
 		});
 
 		super.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		
+		// Register the authentication broadcast receiver 
+		IntentFilter filter = new IntentFilter();
+		filter.addAction( Connector.AUTH_INTENT );
+		registerReceiver( mAuthReceiver, filter );
+	}
+	
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+		if( scanResult != null ) {
+			// handle scan result
+			String key = scanResult.getContents();
+			try	{
+				
+				String address = key.substring(0,8);
+				String security_key = key.substring(8);
+				
+				int secret_key = Utility.HexStringToInteger(security_key);
+				InetAddress serverAddress = InetAddress.getByAddress(Utility.HexStringToByteArray(address));
+				
+				Connector c = Connector.getInstance(this);
+				c.ConnectToServer(serverAddress, secret_key);
+			}
+			catch (UnknownHostException uhe) {
+				uhe.printStackTrace();
+			}
+		}
+		// else continue with any other code you need in the method
 	}
 
 }
